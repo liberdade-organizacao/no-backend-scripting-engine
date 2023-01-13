39e1ff719dcbfd9a2ff426540dd4b561e1a6353e
Cristiano Alves da Silva Junior <crisjr@pm.me>
2023-01-13 10:23:44 -0300

    Convert shell integration test into a Go unit test

--- integration/integration_test.sh
+++ integration/integration_test.sh
@@ -4,8 +4,6 @@ set -e
 
 echo 'Ensure both app and database are running first!'
 
-# TODO turn this into an actual unit test
-
 # creating required data
 alias psqlcmd='psql -h localhost -p 5434 -d baas -U liberdade -c' 
 
@@ -14,6 +12,7 @@ lua_script=`cat test_action_script.lua`
 
 psqlcmd "INSERT INTO clients(email, password, is_admin) VALUES('test@go.dev','password','off') ON CONFLICT DO NOTHING;"
 psqlcmd "INSERT INTO apps(owner_id,name) VALUES(1,'go test app') ON CONFLICT DO NOTHING;"
+psqlcmd "INSERT INTO app_memberships(app_id,client_id,role) VALUES(1,1,'admin') ON CONFLICT DO NOTHING RETURNING *;"
 psqlcmd "INSERT INTO users(app_id,email,password) VALUES(1,'test@go.dev','password') ON CONFLICT DO NOTHING;"
 psqlcmd "INSERT INTO actions(app_id,name,script) VALUES (1,'Test Action','') ON CONFLICT DO NOTHING;"
 psqlcmd "UPDATE actions SET script='$lua_script' WHERE id='1';"
