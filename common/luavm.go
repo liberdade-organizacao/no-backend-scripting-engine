a31e0c0f64841542bf55896441d8887d7302914d
Cristiano Alves da Silva Junior <crisjr@pm.me>
2023-01-13 11:36:55 -0300

    Let Lua main functions receive and return strings

--- common/luavm.go
+++ common/luavm.go
@@ -1,7 +1,6 @@
 package common
 
 import (
-    "fmt"
     "github.com/yuin/gopher-lua"
     "liberdade.bsb.br/baas/scripting/database"
 )
@@ -12,28 +11,36 @@ import (
 // TODO include functino to delete fiels for a user
 // TODO include function to check if a file exists
 // TODO include upsert function
-// TODO if possible, get return value from lua script 
 
-const LUA_TEMPLATE=`
-
-%s
-
-main(%s)
-`
-
+// Runs a raw Lua script
 func RunLua(script string) error {
-    L := lua.NewState()
-    defer L.Close()
-    return L.DoString(script)
+	L := lua.NewState()
+	defer L.Close()
+	return L.DoString(script)
 }
 
-func RunLuaMain(actionScript string, connection *database.Conn) error {
-    L := lua.NewState()
-    defer L.Close()
+// Runs a main function from Lua and returns its result
+// This Lua main function must receive a string and return a string.
+func RunLuaMain(actionScript string, inputData string, connection *database.Conn) (string, error) {
+	L := lua.NewState()
+	defer L.Close()
     
-    // TODO use parameters that were sent through the request
-    params := "\"nothing yet\""
-    script := fmt.Sprintf(LUA_TEMPLATE, actionScript, params)
-    return L.DoString(script)
+	err := L.DoString(actionScript)
+	if err != nil {
+		return "", err
+	}
+
+	err = L.CallByParam(lua.P{
+		Fn: L.GetGlobal("main"),
+		NRet: 1,
+		Protect: true,
+	}, lua.LString(inputData))
+	if err != nil {
+		return "", err
+	}
+	ret := L.Get(-1)
+	L.Pop(1)
+
+	return ret.String(), nil
 }
 
